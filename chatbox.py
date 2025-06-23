605d93cef8410575c28c129370a8ff8dcdf4fa2a
vpavankumarreddy8 <www.pkumar873@gmail.com>
2025-06-23 20:10:03 +0530

    Add files via upload

---
+++ chatbox.py
@@ -0,0 +1,18 @@
+def chatbot():
+    print("Welcome to Chatbot! (type 'bye' to exit)")
+
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("Bot: Hi!")
+        elif user_input == "how are you":
+            print("Bot: I'm fine, thanks!")
+        elif user_input == "bye":
+            print("Bot: Goodbye!")
+            break
+        else:
+            print("Bot: Sorry, I don't understand.")
+
+# Run the chatbot
+chatbot()
